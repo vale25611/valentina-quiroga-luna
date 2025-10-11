a4ec2f3240bd9af5b2f1b0fbcb57026afa541e02
vale25611 <vquiroga25611@gmail.com>
2025-10-10 20:57:32 -0400

    js added, created footer and skills

---
+++ js/index.js
@@ -0,0 +1,48 @@
+const body = document.querySelector("body");
+
+//footer
+const footer = document.createElement("footer");
+footer.id = "copyright";
+body.appendChild(footer);
+
+const today = new Date();
+const thisYear = today.getFullYear();
+
+const copyright = document.createElement("p");
+copyright.innerHTML =`© ${thisYear} Valentina Quiroga`;
+footer.appendChild(copyright);
+
+
+const skills = [
+  "JavaScript",
+  "HTML",
+  "CSS",
+  "React Native (Expo)",
+  "Swift (iOS)",
+  "Firebase",
+  "Python",
+  "C++",
+  "Git",
+  "GitHub",
+  "Linux",
+  "VS Code",
+  "AWS (Cloud9)",
+  "DynamoDB",
+  "Gemini AI",
+  "Adobe Photoshop",
+  "Adobe Illustrator",
+  "Adobe InDesign",
+  "English (Fluent)",
+  "Spanish (Fluent)"
+];
+
+
+const skillsSection = document.getElementById("skills");
+const skillsList = skillsSection.querySelector("ul")
+
+for(let i = 0; i < skills.length; i++){
+    const skill = document.createElement("li");
+    skill.innerHTML = `${skills[i]}`;
+    skillsList.appendChild(skill)
+}
+
